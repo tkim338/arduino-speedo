bb4e4e132e43c601546c3c9546d9b76a008a5e8b
Thomas Kim <tjk103@gmail.com>
2023-07-07 11:00:21 -0700

    clean up

--- integration.ino
+++ integration.ino
@@ -1,26 +1,29 @@
 #include <Servo.h>
 
-int hall_pin = 2;
-float rpm;
-float duration;
+int hall_sensor_pin = 2;
+int servo_output_pin = 3;
+
+// calibration values
+unsigned long pulse_ratio = 0.1;  // proportion of rotation during which sensor is activated
+int max_rpm = 7500;  // rpm of driveshaft that correlates to maximum speed on gauge
 
 Servo speedo_servo;
-int angle;
 
 void setup() {
   Serial.begin(9600);
-  // make the hall pin an input:
-  pinMode(hall_pin, INPUT);
-  speedo_servo.attach(9);
+  pinMode(hall_sensor_pin, INPUT);
+  speedo_servo.attach(servo_output_pin);
 }
 
 void loop() {
-  duration = pulseIn(hall_pin, FALLING, 500000); //times the amount of microseconds the motor is not timing IR, Times out after 100000 uS. Raise the timeout for slower RPM readings. .5 second
-  rpm = 60.0/duration*100000; //See above
-  if (duration == 0) {
+  int timeout = 1000000;  // uS (1 sec); min 60 RPM measurement
+  unsigned long pulse_duration = pulseIn(hall_sensor_pin, HIGH, timeout) / 1000000;  // seconds
+  unsigned long rps = pulse_ratio / pulse_duration;
+  unsigned long rpm = rps * 60;
+  if (duration == 0) {  // This avoids division by 0 that results in rpm = inf
     rpm = 0;
   }
-  angle = map(rpm, 0, 7435, 0, 179);
+  int angle = map(rpm, 0, max_rpm, 0, 179);
   Serial.print(rpm);
   Serial.print(", ");
   Serial.println(angle);
