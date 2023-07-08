06d3a70f3e560132e042e7839c3a3cfda1474089
Thomas Kim <tjk103@gmail.com>
2023-07-08 08:21:18 -0700

    auto calibration fix

--- arduino-speedo.ino
+++ arduino-speedo.ino
@@ -3,8 +3,10 @@
 int hall_sensor_pin = 2;
 int servo_output_pin = 3;
 
+unsigned long prev_pulse_end = 0.0;
+
 // calibration values
-int max_rpm = 5850;  // rpm of driveshaft at max measured speed
+int max_rpm = 5850;  // rpm of driveshaft that correlates to maximum speed on gauge
 
 Servo speedo_servo;
 
@@ -15,24 +17,16 @@ void setup() {
 }
 
 void loop() {
-  unsigned long timeout = 100000;  // uS (0.1 sec)
+  unsigned long timeout = 200000;  // uS (1 sec); min 60 RPM measurement
+  unsigned long pulse_duration = pulseIn(hall_sensor_pin, HIGH, timeout);
+  unsigned long rotation_period = micros() - prev_pulse_end; // uS
+  prev_pulse_end = micros();
 
-  unsigned long start_time = micros();
-  int pulse_count = 0;
-  while (micros() < start_time + timeout) {
-    bool pulse_counted = false;
-    while (digitalRead(hall_sensor_pin) == HIGH) {
-      if (pulse_counted == false) {
-        pulse_counted = true;
-        pulse_count += 1;
-      }
-    }
-  }
-
-  unsigned long rps = pulse_count / timeout / 1000000;
+  unsigned long rps = 1.0 / (rotation_period / 1000000.0);
   unsigned long rpm = rps * 60;
-  pulse_count = 0;
-
+  if (pulse_duration == 0) {
+    rpm = 0;
+  }
   int angle = map(rpm, 0, max_rpm, 0, 179);
   Serial.print(rpm);
   Serial.print(", ");
