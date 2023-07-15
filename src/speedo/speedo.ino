b5d055578dc00eab6efcb1e734b1bc2f9387d833
Thomas Kim <tjk103@gmail.com>
2023-07-15 16:11:55 -0700

    rounding fix

--- src/speedo/speedo.ino
+++ src/speedo/speedo.ino
@@ -4,8 +4,8 @@ int hall_sensor_pin = 2;
 int servo_output_pin = 3;
 
 unsigned long prev_pulse_end;
-unsigned long pulse_on_ratio;
-unsigned long rpm;
+float pulse_on_ratio;
+float rpm;
 
 // calibration values
 int max_rpm = 7800;  // rpm of driveshaft that correlates to maximum speed on gauge (100 mph)
@@ -19,22 +19,23 @@ void setup() {
 }
 
 void loop() {
-  unsigned long timeout = 200000;  // uS (1 sec); min 60 RPM measurement
+  unsigned long timeout = 200000;  // uS (0.2 sec); min 12 RPM measurement
   unsigned long pulse_duration = pulseIn(hall_sensor_pin, HIGH, timeout);
   unsigned long rotation_period = micros() - prev_pulse_end; // uS
   prev_pulse_end = micros();
-
+  
   if (pulse_duration == 0) {
     rpm = 0;
   }
   else {
-    pulse_on_ratio = pulse_duration / rotation_period;
+    pulse_on_ratio = float(pulse_duration) / rotation_period;
     rpm = pulse_on_ratio / (pulse_duration / 1000000.0) * 60;
   }
   
   int angle = map(rpm, 0, max_rpm, 0, 179);
+  Serial.print("RPM: ");
   Serial.print(rpm);
-  Serial.print(", ");
+  Serial.print(", angle: ");
   Serial.println(angle);
   speedo_servo.write(angle);
 }
