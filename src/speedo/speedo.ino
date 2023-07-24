2a6baa160291f858a2ca56f246a673628a5d1ff2
Thomas Kim <tjk103@gmail.com>
2023-07-23 21:08:12 -0700

    refactored speedo code

--- src/speedo/speedo.ino
+++ src/speedo/speedo.ino
@@ -5,10 +5,15 @@ int servo_output_pin = 3;
 
 unsigned long prev_pulse_end;
 float rpm;
+int prev_pulse_count;
+int pulse_count;
+unsigned long prev_time;
+unsigned long pulse_duration;
+
+unsigned long timeout = 500000;  // uS (0.5 sec)
 
 // calibration values
 float max_rpm = 7800;  // rpm of driveshaft that correlates to maximum speed on gauge (100 mph)
-float pulse_on_ratio = 0.1;  // calibrate this value
 
 Servo speedo_servo;
 
@@ -19,18 +24,17 @@ void setup() {
 }
 
 void loop() {
-  unsigned long timeout = 1000000;  // uS (1 sec)
-  unsigned long pulse_duration = pulseIn(hall_sensor_pin, HIGH, timeout);
-  unsigned long rotation_period = micros() - prev_pulse_end; // uS
-  prev_pulse_end = micros();
-  
-  if (pulse_duration == 0) {
-    rpm = 0;
+  while (micros() < prev_time + timeout) {
+    pulse_duration = pulseIn(hall_sensor_pin, HIGH, timeout);
+    if (pulse_duration > 0) {
+      pulse_count += 1;
+    }
   }
-  else {
-    pulse_on_ratio = float(pulse_duration) / rotation_period;
-    rpm = pulse_on_ratio / (pulse_duration / 1000000.0) * 60;
-  }
+  prev_time = micros();
+
+  pulse_count = (prev_pulse_count + pulse_count) / 2; // truncated to int; round down
+  rpm = pulse_count / (timeout / 1000000.0);
+  prev_pulse_count = pulse_count;
   
   int angle = map(rpm, 0, max_rpm, 0, 179);
   Serial.print("RPM: ");
