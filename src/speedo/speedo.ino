cc426fc713e1a753f81658a9588e9d15c43dbfbb
Thomas Kim <tjk103@gmail.com>
2023-09-29 20:27:13 -0700

    speedo code fix

--- src/speedo/speedo.ino
+++ src/speedo/speedo.ino
@@ -16,7 +16,7 @@ unsigned long timeout = 200000;  // uS (0.5 sec)
 float diff_ratio = 3.909;
 float wheel_diameter = 24.7;  // inches
 float speedo_max = 100.0;  // mph
-float mile_to_inch = 6336.0;  // inch/mile
+float mile_to_inch = 63360.0;  // inch/mile
 float hr_to_min = 60.0;  // min/hr
 float max_rpm = (speedo_max * mile_to_inch / hr_to_min) / (wheel_diameter * PI) * diff_ratio;  // rpm of driveshaft that correlates to maximum speed on gauge (100 mph)
 
@@ -38,15 +38,22 @@ void loop() {
   prev_time = micros();
 
   pulse_count = (prev_pulse_count + pulse_count) / 2; // truncated to int; round down
-  rpm = 60.0 * pulse_count / (timeout / 1000000.0);  // [sec/min * unitless / (u_sec / (sec / u_sec))]
-  prev_pulse_count = pulse_count;
+  
+  if (prev_pulse_count > 0 && pulse_count > 0) {
+    rpm = 60.0 * pulse_count / (timeout / 1000000.0);  // [sec/min * unitless / (u_sec / (sec / u_sec))]
+  }
+  else {
+    rpm = 0;
+  }
   
   int angle = map(rpm, 0, max_rpm, 0, 179);
   Serial.print("RPM: ");
   Serial.print(rpm);
   Serial.print(", angle: ");
   Serial.println(angle);
+
   speedo_servo.write(angle);
 
+  prev_pulse_count = pulse_count;
   pulse_count = 0;
 }
